7e7f8495e54adf318c4637cd591a9cab601b256e
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-11-17 07:34:21 +0500

    make failure jacocoTestCoverageVerification

--- src/test/java/org/ot5usk/BoundaryServiceTest.java
+++ src/test/java/org/ot5usk/BoundaryServiceTest.java
@@ -15,15 +15,15 @@ class BoundaryServiceTest {
     private final BoundaryService boundaryService = new BoundaryService();
 
 
-    @DisplayName("Finding the min mark")
-    @Description("Must return the min mark")
-    @ParameterizedTest(name = "from {0}")
-    @MethodSource("arraysOfMarks")
-    void testFindMin(int[] marks) {
-        assertEquals(Arrays.stream(marks).min().getAsInt(), boundaryService.findMin(marks));
-    }
-
-    static Stream<int[]> arraysOfMarks() {
-        return Stream.of(new int[]{1, 1, 0, 2, 0, 7, 7, 15}, new int[]{-1, 1, 0, 0, 11, -11, -1, 9});
-    }
+//    @DisplayName("Finding the min mark")
+//    @Description("Must return the min mark")
+//    @ParameterizedTest(name = "from {0}")
+//    @MethodSource("arraysOfMarks")
+//    void testFindMin(int[] marks) {
+//        assertEquals(Arrays.stream(marks).min().getAsInt(), boundaryService.findMin(marks));
+//    }
+//
+//    static Stream<int[]> arraysOfMarks() {
+//        return Stream.of(new int[]{1, 1, 0, 2, 0, 7, 7, 15}, new int[]{-1, 1, 0, 0, 11, -11, -1, 9});
+//    }
 }
